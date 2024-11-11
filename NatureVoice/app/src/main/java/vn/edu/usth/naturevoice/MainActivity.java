9b99860625c01f98e8400fa9f83ec57d4eec4547
NgocNguyen1103 <quangquangngoc@gmail.com>
2024-11-11 23:12:32 +0700

    Creating Botton NavBar, and link fragments

--- NatureVoice/app/src/main/java/vn/edu/usth/naturevoice/MainActivity.java
+++ NatureVoice/app/src/main/java/vn/edu/usth/naturevoice/MainActivity.java
@@ -4,21 +4,53 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import vn.edu.usth.naturevoice.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.BottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            } else if (itemId == R.id.newPlant) {
+                replaceFragment(new NewPlantFragment());
+                return true;
+            } else if (itemId == R.id.camera) {
+                replaceFragment(new CameraFragment());
+                return true;
+            } else if (itemId == R.id.menu) {
+                replaceFragment(new MenuFragment());
+                return true;
+            }
+            return false;
         });
+        EdgeToEdge.enable(this);
+
+
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+    ;
 }
