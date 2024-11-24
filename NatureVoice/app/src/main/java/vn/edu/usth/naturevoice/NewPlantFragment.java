dde14a8f9ca017663b0efb13c4ab7f47e8fd8c2d
nghia nguyen <nghia@gmail.com>
2024-11-24 23:17:20 +0700

    ui

--- NatureVoice/app/src/main/java/vn/edu/usth/naturevoice/NewPlantFragment.java
+++ NatureVoice/app/src/main/java/vn/edu/usth/naturevoice/NewPlantFragment.java
@@ -3,9 +3,7 @@ package vn.edu.usth.naturevoice;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
